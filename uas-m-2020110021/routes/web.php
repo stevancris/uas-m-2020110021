1968fda8d4635bb6df8c6d8230fdd01adf607d84
stevancris <cristian.stevanus@reku.id>
2024-01-12 19:12:23 +0700

    add route to account, app and transaction controller

--- uas-m-2020110021/routes/web.php
+++ uas-m-2020110021/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\AppController;
+use App\Http\Controllers\TransactionController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AppController::class, 'index'])->name('apps.index');
+Route::get('/accounts', [AccountController::class, 'index'])->name('accounts.index');
+Route::get('/accounts/create', [AccountController::class, 'create'])->name('accounts.create');
+Route::post('/accounts', [AccountController::class, 'store'])->name('accounts.store');
+Route::get('/accounts/{list}', [AccountController::class, 'show'])->name('accounts.show');
+Route::get('/accounts/{list}/edit', [AccountController::class, 'edit'])->name('accounts.edit');
+Route::patch('/accounts/{list}', [AccountController::class, 'update'])->name('accounts.update');
+Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+Route::get('/transactions/create', [TransactionController::class, 'create'])->name('transactions.create');
