147223c16cb30ba228cd8609fa6ba78ff8571537
stevancris <cristian.stevanus@reku.id>
2024-01-15 21:47:21 +0700

    add migration down when rollback alter foreign key

--- uas-m-2020110021/database/migrations/2024_01_12_115533_alter_transactions_table.php
+++ uas-m-2020110021/database/migrations/2024_01_12_115533_alter_transactions_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('transactions', function (Blueprint $table) {
-            //
+            $table->dropForeign(['account_id']);
         });
     }
 };
