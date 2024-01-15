06329646f16e0b827ad66a63f06dc66c4d2b91d6
stevancris <cristian.stevanus@reku.id>
2024-01-15 21:49:53 +0700

    change the type data of id on account and transaction table

--- uas-m-2020110021/database/migrations/2024_01_12_114555_create_accounts_table.php
+++ uas-m-2020110021/database/migrations/2024_01_12_114555_create_accounts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('accounts', function (Blueprint $table) {
-            $table->char('id',16)->index();
+            $table->char('id',16)->primary();
             $table->string('nama');
             $table->string('jenis', 50);
             $table->timestamps();
--- uas-m-2020110021/database/migrations/2024_01_12_114604_create_transactions_table.php
+++ uas-m-2020110021/database/migrations/2024_01_12_114604_create_transactions_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('transactions', function (Blueprint $table) {
-            $table->integer('id')->autoIncrement();
+            $table->id();
             $table->string('kategori',50);
             $table->double('nominal');
             $table->string('tujuan');
