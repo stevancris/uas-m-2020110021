79d77629d568bb52692e287330a1170d436de3bb
stevancris <cristian.stevanus@reku.id>
2024-01-16 00:14:30 +0700

    update the database migration up of table account

--- uas-m-2020110021/database/migrations/2024_01_12_114555_create_accounts_table.php
+++ uas-m-2020110021/database/migrations/2024_01_12_114555_create_accounts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('accounts', function (Blueprint $table) {
-            $table->char('id',16)->primary()->default('0000000000000000')->autoincrement();
+            $table->char('id',16)->primary();
             $table->string('nama');
             $table->string('jenis', 50);
             $table->timestamps();
